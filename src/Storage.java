35c1a145672c29c3344e4f1e05f4504782313979
Quek Jie Ping <sufer197@gmail.com>
2014-09-28 14:18:38 +0800

    Write the writeToFile method for the storage class.

--- src/Storage.java
+++ src/Storage.java
@@ -7,24 +7,49 @@ import org.json.simple.parser.JSONParser;
 
 public class Storage {
 	//temporary  array list to work with
-	public static ArrayList<Task> tempArrList = new ArrayList<Task>();
+	public static ArrayList<Task> globalArrList = new ArrayList<Task>();
 
-	public static boolean readFromFile(String fileName){
+	public static boolean readFromFile(String file){
 		JSONParser parser= new JSONParser();
 		try{
-			Object obj=parser.parse(new FileReader(fileName));
+			Object obj=parser.parse(new FileReader(file));
 			JSONArray jsonArr=(JSONArray)obj;
 			Iterator<JSONObject> iterator=jsonArr.iterator();
 			while(iterator.hasNext()){
 				JSONObject jObj= iterator.next();
 				Task temp= new Task(jObj.get("Name").toString(),jObj.get("ID").toString(),jObj.get("Status").toString(),jObj.get("Deadline").toString(),jObj.get("Workload").toString(),jObj.get("Description").toString());
-				tempArrList.add(temp);
+				globalArrList.add(temp);
 			}
 		}catch(Exception e){
 			e.printStackTrace();
 			return false;
 		}
 		return true;	
+	}
+
+	public static boolean writeToFile(String file){
+
+		JSONArray objArr= new JSONArray();
+		try{
+			FileWriter fw= new FileWriter(file);
+
+			for(int index=0;index<globalArrList.size();index++){
+				JSONObject obj= new JSONObject();
+				obj.put("Name", globalArrList.get(index).getTaskName());
+				obj.put("ID", globalArrList.get(index).getTaskID());
+				obj.put("Status", globalArrList.get(index).getTaskStatus());
+				obj.put("Deadline", globalArrList.get(index).getTaskDeadLine());
+				obj.put("Workload", globalArrList.get(index).getTaskWorkLoad());
+				obj.put("Description", globalArrList.get(index).getTaskDescription());
+				objArr.add(obj);
+			}
+			fw.write(objArr.toJSONString());
+			fw.close();
+		}catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 
 }
