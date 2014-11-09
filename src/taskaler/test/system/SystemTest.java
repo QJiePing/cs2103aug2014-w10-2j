aa7b24eea8badd00544a83c166437a72b9e84159
Quek Jie Ping <surfer197@gmail.com>
2014-11-09 14:36:21 +0800

    add more test case to system test.

--- src/taskaler/test/system/SystemTest.java
+++ src/taskaler/test/system/SystemTest.java
@@ -1,6 +1,7 @@
 package taskaler.test.system;
 
 import static org.junit.Assert.*;
+import taskaler.common.configurations.Configuration;
 import taskaler.common.data.DeadLineTask;
 import taskaler.common.data.FloatTask;
 import taskaler.common.data.RepeatedTask;
@@ -23,6 +24,10 @@ import org.junit.Test;
 //@author A0111798X
 
 public class SystemTest {
+	
+	/**
+	 * The file name should be the same as the file name state in the configuration file.
+	 */
     private static final String TASK_LIST_FILE = "task_list";
 
     @Rule
@@ -32,7 +37,7 @@ public class SystemTest {
 
     @After
     public void tearDownAfterClass() throws Exception {
-        File f = new File(TASK_LIST_FILE);
+        File f = new File(".\\taskaler\\"+TASK_LIST_FILE);
         if (f.exists()) {
            f.delete();
         }
@@ -55,54 +60,52 @@ public class SystemTest {
      * the deadline task to changedTask3. Then, delete one float task. Execute
      * undo command.
      * 
-     * Boolean switch1: boolean value of whether the deadline task name is
-     * changed. Boolean switch2: boolean value of whether there exist 2 float
-     * tasks and check the task name is still the same.
+     * Equivalence Partition: valid command
+     * Boundary Analysis: more than 1 tasks
      * 
      * Expected result: There are 2 float tasks and an edited deadline task with
      * the name changedTask3.
      */
-    @Test
-    public void test1() {
-        try {
-            controller.start(new Stage());
-        } catch (IOException e) {
-            fail(e.getMessage());
-        }
-        boolean switch1 = false;
-        boolean switch2 = false;
-        String cmd1 = "add task1 :It is a float task";
-        String cmd2 = "add task2 :It is a float task2";
-        String cmd3 = "add task3 :It is a deadline task, 29/10/2014, 2";
-        String cmd4 = "edit changedTask3";
-        String cmd5 = "delete 1";
-        String cmd6 = "undo";
+	@Test
+	public void test1() {
+		try {
+			controller.start(new Stage());
+		} catch (IOException e) {
+			fail(e.getMessage());
+		}
+		boolean switch1 = false;
+		boolean switch2 = false;
+		String cmd1 = "add task1 :It is a float task";
+		String cmd2 = "add task2 :It is a float task2";
+		String cmd3 = "add task3 :It is a deadline task, 29/10/2014, 2";
+		String cmd4 = "edit changedTask3";
+		String cmd5 = "delete 1";
+		String cmd6 = "undo";
 
-        controller.executeCMD(cmd1);
-        controller.executeCMD(cmd2);
-        controller.executeCMD(cmd3);
-        controller.executeCMD(cmd4);
-        controller.executeCMD(cmd5);
-        controller.executeCMD(cmd6);
+		controller.executeCMD(cmd1);
+		controller.executeCMD(cmd2);
+		controller.executeCMD(cmd3);
+		controller.executeCMD(cmd4);
+		controller.executeCMD(cmd5);
+		controller.executeCMD(cmd6);
 
-        TaskAndConfigStorage storeObj = TaskAndConfigStorage.getInstance();
-        ArrayList<Object> tempArr = storeObj.readFromFile(TASK_LIST_FILE);
-        ArrayList<FloatTask> floatArr = (ArrayList<FloatTask>) tempArr.get(0);
-        ArrayList<DeadLineTask> deadlineArr = (ArrayList<DeadLineTask>) tempArr
-                .get(1);
+		TaskAndConfigStorage storeObj = TaskAndConfigStorage.getInstance();
+		ArrayList<Object> tempArr = storeObj.readFromFile(TASK_LIST_FILE);
+		ArrayList<FloatTask> floatArr = (ArrayList<FloatTask>) tempArr.get(0);
+		ArrayList<DeadLineTask> deadlineArr = (ArrayList<DeadLineTask>) tempArr
+				.get(1);
 
-        if (deadlineArr.get(0).getTaskName().equals("changedTask3")) {
-            switch1 = true;
-        }
+		if (deadlineArr.get(0).getTaskName().equals("changedTask3")) {
+			switch1 = true;
+		}
 
-        if (floatArr.size() == 2) {
-            if (floatArr.get(0).getTaskName().equals("task1")
-                    && floatArr.get(1).getTaskName().equals("task2")) {
-                switch2 = true;
-            }
-        }
-
-        assertTrue((switch1 && switch2));
+		if (floatArr.size() == 2) {
+			if (floatArr.get(0).getTaskName().equals("task1")
+					&& floatArr.get(1).getTaskName().equals("task2")) {
+				switch2 = true;
+			}
+		}
+		assertTrue((switch1 && switch2));
         /**
          * Clear the file content for other test case
          */
@@ -116,46 +119,143 @@ public class SystemTest {
      * 
      * Test case: Executes any invalid command
      * 
+     * Equivalence Partition: Invalid Command
+     * Boundary Analysis: 0 task
+     * 
      * Expected result: There should not have any changes to the tasklist and
      * the components should not crash
      */
     @Test
-    public void test2() {
-        try {
-            controller.start(new Stage());
-        } catch (IOException e) {
-            fail(e.getMessage());
-        }
+	public void test2() {
+		try {
+			controller.start(new Stage());
+		} catch (IOException e) {
+			fail(e.getMessage());
+		}
 
-        String cmd1 = "illegal command";
+		String cmd1 = "illegal command";
 
-        controller.executeCMD(cmd1);
+		controller.executeCMD(cmd1);
 
-        TaskAndConfigStorage storeObj = TaskAndConfigStorage.getInstance();
-        TaskList taskList=TaskList.getInstance();
-        taskList.clear();
-        storeObj.writeToFile(TASK_LIST_FILE, taskList);
-        
-        ArrayList<Object> tempArr = storeObj.readFromFile(TASK_LIST_FILE);
-        ArrayList<FloatTask> floatArr = (ArrayList<FloatTask>) tempArr.get(0);
-        ArrayList<DeadLineTask> deadlineArr = (ArrayList<DeadLineTask>) tempArr
-                .get(1);
-        ArrayList<RepeatedTask> repeatedArr = (ArrayList<RepeatedTask>) tempArr
-                .get(2);
+		TaskAndConfigStorage storeObj = TaskAndConfigStorage.getInstance();
+		ArrayList<Object> tempArr = storeObj.readFromFile(TASK_LIST_FILE);
+		ArrayList<FloatTask> floatArr = (ArrayList<FloatTask>) tempArr.get(0);
+		ArrayList<DeadLineTask> deadlineArr = (ArrayList<DeadLineTask>) tempArr
+				.get(1);
+		ArrayList<RepeatedTask> repeatedArr = (ArrayList<RepeatedTask>) tempArr
+				.get(2);
 
-        if (floatArr.size() == 0 && deadlineArr.size() == 0
-                && repeatedArr.size() == 0) {
-            assertTrue("Passed", true);
-        } else {
-            fail("Failed illegal command test");
-        }
+		if (floatArr.size() == 0 && deadlineArr.size() == 0
+				&& repeatedArr.size() == 0) {
+			assertTrue(true);
+		} else {
+			fail("Failed illegal command test");
+		}
 
-    }
+	}
     /**
      * Integration 3
+     * 
+     * Test Case: Execute an empty command
+     * 
+     * Equivalence Partition: Empty Command
+     * Boundary Analysis: 0 task
+     * 
+     * Expected Result: No changes are made to TaskList and program should not crash.
      */
     @Test
-    public void test3(){
-    	
-    }
+	public void test3() {
+		try {
+			controller.start(new Stage());
+		} catch (IOException e) {
+			fail(e.getMessage());
+		}
+
+		String cmd1 = " ";
+
+		TaskAndConfigStorage storeObj = TaskAndConfigStorage.getInstance();
+		ArrayList<Object> tempArr = storeObj.readFromFile(TASK_LIST_FILE);
+		ArrayList<FloatTask> floatArr = (ArrayList<FloatTask>) tempArr.get(0);
+		ArrayList<DeadLineTask> deadlineArr = (ArrayList<DeadLineTask>) tempArr
+				.get(1);
+		ArrayList<RepeatedTask> repeatedArr = (ArrayList<RepeatedTask>) tempArr
+				.get(2);
+
+		int floatNum = floatArr.size();
+		int deadlineNum = deadlineArr.size();
+		int repeatedNum = repeatedArr.size();
+
+		controller.executeCMD(cmd1);
+
+		storeObj = TaskAndConfigStorage.getInstance();
+		tempArr = storeObj.readFromFile(TASK_LIST_FILE);
+		floatArr = (ArrayList<FloatTask>) tempArr.get(0);
+		deadlineArr = (ArrayList<DeadLineTask>) tempArr.get(1);
+		repeatedArr = (ArrayList<RepeatedTask>) tempArr.get(2);
+
+		if (floatArr.size() == floatNum && deadlineArr.size() == deadlineNum
+				&& repeatedArr.size() == repeatedNum) {
+			assertTrue(true);
+		} else {
+			fail("Failed empty command test");
+		}
+	}
+    /**
+     * Integration test 4
+     * 
+     * Test case: Add 1 float task, 1 deadline task and 1 repeated task. Change the task name of
+     * the repeated task to changedTaskName. Then, delete the repeated task. Execute undo command.
+     * 
+     * Equivalence Partition: valid command
+     * Boundary Analysis: 1 task in each arraylist
+     * 
+     * Expected result: There are 1 float task, 1 deadline task and 1 edited repeated task with
+     * the name changedTaskName.
+     */
+	@Test
+	public void test4() {
+		try {
+			controller.start(new Stage());
+		} catch (IOException e) {
+			fail(e.getMessage());
+		}
+
+		String cmd1 = "add task1 :It is a float task";
+		String cmd2 = "add task2 :It is a deadline task, 29/10/2014, 2";
+		String cmd3 = "add task3 :It is a repeated task, 3/11/2014, 3";
+		String cmd4 = "repeat 3 monday, to 24/11/2014";
+		String cmd5 = "edit changedTaskName";
+		String cmd6 = "delete 3";
+		String cmd7 = "undo";
+
+		controller.executeCMD(cmd1);
+		controller.executeCMD(cmd2);
+		controller.executeCMD(cmd3);
+		controller.executeCMD(cmd4);
+		controller.executeCMD(cmd5);
+		controller.executeCMD(cmd6);
+		controller.executeCMD(cmd7);
+
+		TaskAndConfigStorage storeObj = TaskAndConfigStorage.getInstance();
+		ArrayList<Object> tempArr = storeObj.readFromFile(TASK_LIST_FILE);
+		ArrayList<FloatTask> floatArr = (ArrayList<FloatTask>) tempArr.get(0);
+		ArrayList<DeadLineTask> deadlineArr = (ArrayList<DeadLineTask>) tempArr
+				.get(1);
+		ArrayList<RepeatedTask> repeatedArr = (ArrayList<RepeatedTask>) tempArr
+				.get(2);
+		if (floatArr.get(0).getTaskName().equals("task1")
+				&& deadlineArr.get(0).getTaskName().equals("task2")
+				&& repeatedArr.get(0).getTaskName().equals("changedTaskName")) {
+			assertTrue(true);
+		} else {
+			fail("Integration test case 4 failed");
+		}
+
+		/**
+		 * Clear the file content for other test case
+		 */
+		TaskList taskList = TaskList.getInstance();
+		taskList.clear();
+		storeObj.writeToFile(TASK_LIST_FILE, taskList);
+	}
 }
